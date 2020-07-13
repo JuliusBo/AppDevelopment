daf6886057f89f48536500262c1cdfc1f15f5f01
JuliusBo <68220168+JuliusBo@users.noreply.github.com>
2020-07-13 14:49:26 +0200

    Create 01Datentypen.dart

---
+++ 01Datentypen.dart
@@ -0,0 +1,26 @@
+main() {
+  int integer1 = 1 ;
+  int integer2 = 2;
+  //Schritt 1,2
+  
+  print(integer1) ;
+  print(integer2) ;
+  //Schritt 3
+  
+ double double1 = 1.1 ;
+  double double2 = 2.2;
+  //Schritt 4,5
+  
+  print(double1);
+  print(double2);
+  //Schritt 6
+  
+  String string1 = "String Nr.1" ;
+  String string2 = "String Nr.2" ;
+  //Schritt 7, 8
+  
+  print(string1) ;
+  print(string2);
+  //Schritt 9
+    
+  }
