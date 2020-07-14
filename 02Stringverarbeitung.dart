9558aae28ffc21eb9f4bf54ae5dd3263627f17a2
JuliusBo <68220168+JuliusBo@users.noreply.github.com>
2020-07-14 10:33:21 +0200

    Create 02Stringverarbeitung.dart

---
+++ 02Stringverarbeitung.dart
@@ -0,0 +1,12 @@
+main() {
+   String meinName = "Julius" ;
+  print("Mein Name ist $meinName");
+   
+  double double1 = 1.2;
+   String cast = double1.toString();
+  print (cast);
+  
+  int laenge = meinName.length;
+  print("mein Name ist $laenge Zeichen lang");
+  
+  }
