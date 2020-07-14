c696495a3c2e2ec1efa551b70ca730cf0ba3ceec
JuliusBo <68220168+JuliusBo@users.noreply.github.com>
2020-07-14 10:55:56 +0200

    Create 03Operator.dart

---
+++ 03Operator.dart
@@ -0,0 +1,19 @@
+main() {
+  int zahl = 0;
+zahl= zahl + 2 ;
+  print(zahl) ;
+  zahl= zahl + 5 ;
+  print(zahl) ;
+  
+  zahl = 0 ;
+  
+  zahl++ ; 
+  print(zahl) ;
+  
+  int quadrat = 4 ;
+  
+  quadrat *= quadrat ;
+  
+  print(quadrat);  
+  
+  }
